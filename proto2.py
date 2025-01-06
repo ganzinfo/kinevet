918637bd03466501a9d561636af332d6405a244c
ganzinfo <varga.tamas@h2co3.at>
2025-01-06 10:34:42 +0100

    first steps

---
+++ proto2.py
@@ -0,0 +1,20 @@
+"""
+A játékos saját koordinátarendszere és a tábla koordinátarendszere közötti átváltás megvalósítása
+
+"""
+
+uCoord = {}
+tCoord = {}
+
+for i in range(40):
+    uCoord[i] = []
+    tCoord[i] = []
+
+print(uCoord,"\n", tCoord)
+
+
+def ucoordToTable(): #User coordinate convert to table coordinate
+    pass
+
+def tcoordToUser(): #Table coordinate convert to user coordinate
+    pass
