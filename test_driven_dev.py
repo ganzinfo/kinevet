cdd58d90d68125936f06f83968854daabfa173cc
samat-agrav <varga.tamas@varga.info.hu>
2025-01-14 12:18:39 +0100

    Házi feladat kiírás pontosítás

--- test_driven_dev.py
+++ test_driven_dev.py
@@ -2,27 +2,17 @@
 import TDD_houseCheck
 import pytest
 
-# testdata = set()
-# testdata.add((39,1,(0,2,0,3),-1))
-# testdata.add ((39,2,(0,2,0,3),1))
-# testdata.add ((39,3,(0,2,0,3),1))
-# testdata.add ((39,4,(0,2,0,3),3))
-# testdata.add ((39,5,(0,2,0,3),3))
-# testdata.add ((39,6,(0,2,0,0),4))
-
 #Házi feladat: kétszintű tuple-ba tenni a tesztadatokat.
 
 #@pytest.mark.xfail
-@pytest.mark.parametrize("test_input,expected", [("3+5", 8), ("2+4", 6), ("6*9", 42)])
-def test_houseCheck(startfield, diceRoll, testHouse):
-    for data in testdata:
-        startfield = data[0]
-        diceRoll = data[1]
-        testHouseFields=data[2]
-        expectedValue = data[3]
-        pytest.assume(TDD_houseCheck.houseCheck(startfield, diceRoll, testHouseFields) == expectedValue)
-        print(data)
-
-
-# print(testdata)
-#test_houseCheck()
+@pytest.mark.parametrize("startfield,diceRoll,testHouse,expected ",
+[
+(39,1,(0,2,0,3),0),
+(39,2,(0,2,0,3),1),
+(39,3,(0,2,0,3),1),
+(39,4,(0,2,0,3),3),
+(39,5,(0,2,0,3),3),
+(39,6,(0,2,0,0),4)
+])
+def test_houseCheck(startfield, diceRoll, testHouse, expected):
+    assert(TDD_houseCheck.houseCheck(startfield, diceRoll, testHouse) == expected)
