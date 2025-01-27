17ba3b639195af755e23363500b46ae70c8cb222
ganzinfo <varga.tamas@h2co3.at>
2025-01-27 10:37:20 +0100

    25-01-27_10:35

--- conftest.py
+++ conftest.py
@@ -2,18 +2,18 @@ import pytest
 from table import Table
 # import _global
 
-
+@pytest.fixture
 def tablesetup_PieceKnockOff():
     table = Table()
-
     tupledata = ("A1",0)
     table.table["F"][4] = tupledata
     tupledata = ("B2",0)
     table.table["F"][7] = tupledata
-    print(table.tablePrintout())
+
+    # print(table.tablePrintout())
 
     #table.tablePrintout()
     #table.fullPrintout()
 
 
-tablesetup_PieceKnockOff()
+# tablesetup_PieceKnockOff()
---
+++ test_pieceKnockOff.py
@@ -0,0 +1,10 @@
+import pytest
+import conftest
+
+
+#table Class-ban vagy _globalban?
+
+
+def test_pieceKnockOff(tablesetup_PieceKnockOff):
+    print(table.tablePrintout())
+    # pass
